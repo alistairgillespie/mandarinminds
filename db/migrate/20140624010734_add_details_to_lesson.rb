d63ee4018e0d21189a06e1fc5b5281b2595ea206
Scott Lyle <scott.w.lyle@gmail.com>
2014-06-24 09:33:29 +0800

    removed index line from migration

--- db/migrate/20140624010734_add_details_to_lesson.rb
+++ db/migrate/20140624010734_add_details_to_lesson.rb
@@ -3,6 +3,4 @@ class AddDetailsToLesson < ActiveRecord::Migration
     add_column :lessons, :student_id, :integer
     add_column :lessons, :teacher_id, :integer
   end
-
-  add_index :lessons, [:student_id, :teacher_id]
 end
