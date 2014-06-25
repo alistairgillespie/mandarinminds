455bb492c4d575de364161a66e3c117c384f09f6
WilsonAus <gillaw06@gmail.com>
2014-06-25 15:06:54 +0800

    Routing fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' } 
+  devise_for :users 
     #devise_scope :user do
 	#get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   #end
