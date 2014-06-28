b7de82c082696822b3396766134d2510bebfef29
Scott Lyle <scott.w.lyle@gmail.com>
2014-06-28 11:27:20 +0800

    Changed lesson starts_at to starting_time

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ActiveRecord::Base
-  belongs_to :student, :class_name => 'User', inverse_of: :lessons_to_attend
-  belongs_to :teacher, :class_name => 'User', inverse_of: :lessons_to_teach
+	extend SimpleCalendar
+	has_calendar
+	belongs_to :student, :class_name => 'User', inverse_of: :lessons_to_attend
+	belongs_to :teacher, :class_name => 'User', inverse_of: :lessons_to_teach
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625012318) do
+ActiveRecord::Schema.define(version: 20140628032608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20140625012318) do
   create_table "lessons", force: true do |t|
     t.integer  "student_id"
     t.integer  "teacher_id"
-    t.datetime "starts_at"
+    t.datetime "starting_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
