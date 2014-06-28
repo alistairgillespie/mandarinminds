5ad3190606d4af7066fac8152f2e772878b373a1
Scott Lyle <scott.w.lyle@gmail.com>
2014-06-28 13:55:56 +0800

    Calendar shows booked and free lessons

--- app/views/lessons/index.json.jbuilder
+++ app/views/lessons/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@lessons) do |lesson|
-  json.extract! lesson, :id, :student_id, :teacher_id, :starting_time
+  json.extract! lesson, :id, :student_id, :teacher_id, :starts_at
   json.url lesson_url(lesson, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628035455) do
+ActiveRecord::Schema.define(version: 20140628051849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20140628035455) do
   create_table "lessons", force: true do |t|
     t.integer  "student_id"
     t.integer  "teacher_id"
-    t.datetime "starting_time"
+    t.datetime "starts_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
