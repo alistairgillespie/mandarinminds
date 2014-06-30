591ab72a22ef8729429c253043f1ecc87feba4c7
WilsonAus <gillaw06@gmail.com>
2014-06-30 20:36:45 +0800

    routes tinkering

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,19 @@ Rails.application.routes.draw do
 	#get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   #end
   
+  
+  
+  
+  
   get 'welcome/index'
   # You can have the root of your site routed with "root"
   root 'static#index'
   
+  
+  
+ 
+  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
