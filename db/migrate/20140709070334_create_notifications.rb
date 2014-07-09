fb95329a97295c15d48838494602d0c32deca4a3
Scott Lyle <scott.w.lyle@gmail.com>
2014-07-09 15:04:29 +0800

    Generated notificaton model and scaffold

---
+++ db/migrate/20140709070334_create_notifications.rb
@@ -0,0 +1,13 @@
+class CreateNotifications < ActiveRecord::Migration
+  def change
+    create_table :notifications do |t|
+      t.references :user, index: true
+      t.string :image
+      t.text :content
+      t.boolean :dismissed
+      t.datetime :appear_at
+
+      t.timestamps
+    end
+  end
+end
