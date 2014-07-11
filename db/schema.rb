e858a434ca988e3b12351124d190a320b532663f
Scott Lyle <scott.w.lyle@gmail.com>
2014-07-11 13:56:23 +0800

    Merging

--- db/schema.rb
+++ db/schema.rb
@@ -79,12 +79,7 @@ ActiveRecord::Schema.define(version: 20140711010503) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "role_id"
-<<<<<<< HEAD
-    t.string   "customer_id"
-    t.string   "last_4_digits"
-=======
     t.integer  "lesson_count"
->>>>>>> cf2f99ccf0171e5798972604db1238733bf38d84
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
