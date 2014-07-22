a0dc145926312c4ac5d745082eabbd6d43ceaa29
WilsonAus <gillaw06@gmail.com>
2014-07-22 14:22:51 +0800

    App.js hotfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,13 +9,10 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require underscore
 //= require_tree .
-//=require application.js
-//= require bootstrap
+
 
 
 
