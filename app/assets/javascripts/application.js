07db6f0f48335d79ac514280c07dd0956d3b0f78
WilsonAus <gillaw06@gmail.com>
2014-09-10 13:54:51 +0800

    Static content updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,13 @@
 
 
 
-
+$(document).ready(function() {
+	function scrollToAnchor(aid){
+		var aTag = $("a[name='"+ aid +"']");
+		$('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+	$("a").click(function() {
+		var href = $(this).attr('href').replace('#', '')
+		scrollToAnchor(href);
+	});			
+});
